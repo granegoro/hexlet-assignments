07db01c1486ebb33cd92b75918f6053d65081dfd
@hexlet/cli <support@hexlet.io>
2024-05-27 19:36:35 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -18,18 +18,20 @@ public class PostsController {
 
     private List<Post> posts = Data.getPosts();
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/users/{userId}/posts") //создание нового поста, привязанного к пользователю по id
+    public Post create(@PathVariable Integer userId, @RequestBody Post post) {
+        post.setUserId(userId);
+        posts.add(post);
+        return post;
+    }
+
     @GetMapping("/users/{userId}/posts")
     //список всех постов, написанных пользователем с таким же userId, как id в маршруте
     public List<Post> show(@PathVariable Integer userId) {
         return posts.stream().filter(p -> p.getUserId() == userId).toList();
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/users/{id}/posts") //создание нового поста, привязанного к пользователю по id
-    public Post create(@PathVariable Integer userId, @RequestBody Post post) {
-        post.setUserId(userId);
-        posts.add(post);
-        return post;
-    }
+
 }
 // END
